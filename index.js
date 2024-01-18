864a86db7c90cf8996353cd62282ee36dcb64efc
kotyba-km <kotybaalkassarkm@gmail.com>
2024-01-18 11:11:19 +0200

    launch

---
+++ index.js
@@ -0,0 +1,9 @@
+const menu_btn = document.querySelector(".btn");
+const menu = document.querySelector('.social-menu')
+const close_menu = document.querySelector('.close')
+menu_btn.addEventListener('click',function() {
+  menu.classList.add('is-active')
+});
+close_menu.addEventListener('click' ,function(){
+  menu.classList.remove('is-active')
+})
